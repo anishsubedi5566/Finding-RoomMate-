8a77dcf43c937322c4d58ea98fbf5007d22d5750
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-04-24 16:59:28 -0400

    Route Validation Added

--- routes/posts.js
+++ routes/posts.js
@@ -9,17 +9,36 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    const { postDate, image, sizeOfApartment, petsAllowed, coupleAllowed, parkingAvailable, numberOfRooms,sharingAllowed, City, State, comments } = req.body;
-
+    const { postDate, sizeOfApartment, numberOfRooms, city, state, comments } = req.body 
     try {
-      const output = await postData.createPost(postDate, image, sizeOfApartment, petsAllowed, coupleAllowed, parkingAvailable, numberOfRooms,sharingAllowed, City, State, comments);
+    if(!postDate) throw "postDate is empty"
+    if(!sizeOfApartment || typeof sizeOfApartment != "string" || sizeOfApartment == " ") throw "Enter size of apartment"
+    if(!numberOfRooms || typeof numberOfRooms != "string" || numberOfRooms == " ") throw "Enter number of rooms"
+    if(!city || typeof city != "string" || city == " ") throw "Enter city"
+    if(!state || typeof state != "string" || state == " ")  throw "Enter state"
+    
+    let petsAllowed = coupleAllowed = parkingAvailable = sharingAllowed = false;
+
+    if(req.body.petsAllowed){
+      petsAllowed = true
+    }
+    if(req.body.coupleAllowed){
+      coupleAllowed = true
+    }
+    if(req.body.parkingAvailable){
+      parkingAvailable = true
+    }
+    if(req.body.sharingAllowed){
+      sharingAllowed = true
+    }
+      const output = await postData.createPost(postDate, sizeOfApartment, petsAllowed, coupleAllowed, parkingAvailable, numberOfRooms, sharingAllowed, city, State, comments);
       if (output) {
         res.redirect("/post");
       }
     } catch (e) {
       if (e) {
         const out = { errors: e };
-        res.status(400).render("post");
+        res.status(400).render("post", out);
         return;
       } else {
         res.status(500).json({
@@ -28,9 +47,5 @@ router.post("/", async (req, res) => {
       }
     }
   });
-
-
-
-
 
 module.exports = router;
