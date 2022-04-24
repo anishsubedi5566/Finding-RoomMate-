04817888b84e6c83f70bdfd94965885c4e3a392a
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-04-24 14:59:13 -0400

    Added Post Routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const data = require("../data");
-// const userData = data.users;
+const data = require("../data");
+const postData = data.postData;
 const router = express.Router();
 
 
@@ -8,8 +8,32 @@ router.get("/", async (req, res) => {
     res.render("post");
 });
 
+router.post("/", async (req, res) => {
+    console.log("Reached here",req.body)
+    const { postDate, image, sizeOfApartment, petsAllowed, coupleAllowed, parkingAvailable, numberOfRooms,sharingAllowed, City, State, comments } = req.body;
+    
+    console.log( postDate, image, sizeOfApartment, petsAllowed, coupleAllowed, parkingAvailable, numberOfRooms,sharingAllowed, City, State, comments )
 
-
+    try {
+        console.log("Requesting createpost")
+      const output = await postData.createPost(postDate, image, sizeOfApartment, petsAllowed, coupleAllowed, parkingAvailable, numberOfRooms,sharingAllowed, City, State, comments);
+        console.log("Post Posted", output)
+      if (output) {
+        console.log("Got output", output)
+        res.redirect("/post");
+      }
+    } catch (e) {
+      if (e) {
+        const out = { errors: e };
+        res.status(400).render("post");
+        return;
+      } else {
+        res.status(500).json({
+          error: "Internal Server Error",
+        });
+      }
+    }
+  });
 
 
 
