2a888863c453ecc82e9e0aacb28005d1284e514a
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-04-24 14:00:59 -0400

    Post Frontend Setup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const usersRoute = require("./users");
+const postRoute = require("./posts")
 const constructorMethod = (app) => {
   app.use("/", usersRoute);
+  app.use("/post", postRoute)
   app.use("*", (request, response) => {
     response.status(404).json({
       error: "Not found",
