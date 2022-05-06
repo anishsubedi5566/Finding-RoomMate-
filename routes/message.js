215d2f958ae5889ea5e5540d5a57b6c53f4956e0
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-05-06 09:42:16 -0400

    Routes for message added

--- routes/message.js
+++ routes/message.js
@@ -1,27 +1,39 @@
+
 const express = require("express");
 const constructorMethod = require(".");
 const data = require("../data");
 const postMessage = data.message;
 const router = express.Router();
-const xss = require("xss");
 
 router.get("/", async (req, res) => {
-  const allMessage = await postMessage.getPost(req.session.user.username);
-  //   res.render("post/postRoom", { allpost: allPost });
-  res.render("message/message", { allMessage: allMessage });
+  const allMessage = await postMessage.getMessage(req.session.user.username);
+  console.log("routes/message/message", allMessage[0].sendBy, allMessage[0].receivedBy)
+  
+  res.render("message/message", {allMessage: allMessage,
+   sender: allMessage[0].sendBy,
+     receiver: allMessage[0].receivedBy
+  });
+});
+
+
+router.get("/viewall/:id", async (req, res) => {
+  const allMessage = await postMessage.getSpecificMessage(req.params.id);
+res.render("message/individualMessage", {allMessage: allMessage});
 });
 
 router.post("/", async (req, res) => {
-  let { message, messageTo } = req.body;
-  let sendBy = req.session.user.username;
-  (receivedBy = messageTo), (date = new Date());
+    let {message, messageTo} = req.body
+    let sendBy = req.session.user.username
+    receivedBy = messageTo,
+    date = new Date()
   try {
     const output = await postMessage.createMessage(
-      message,
-      receivedBy,
-      sendBy,
-      date
+        message,
+        receivedBy,
+        sendBy,
+        date
     );
+    console.log("posted routes", output)
     if (output) {
       res.redirect("/private");
     }
